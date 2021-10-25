df75bbb83838cd47ed449e7836b98d0f8b30cbb8
L2HYUNN <dhl9810@naver.com>
2021-10-25 18:40:14 +0900

    #2.0 Canvas Events

---
+++ app.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function onMouseMove(e) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+}
+
+function onMouseDown(e) {
+    painting = true;
+}
+
+function onMouseUp(e) {
+    stopPainting();
+}
+
+function onMouseleave(e) {
+    stopPainting();
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", onMouseleave);
+}
